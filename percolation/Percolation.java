2bad8452222883313ee9b3aa83f1fac9f7ec5413
Max Kharandziuk <max.kharandziuk@gmail.com>
2014-06-25 21:34:59 +0300

    dumb Percolation created

---
+++ percolation/Percolation.java
@@ -0,0 +1,21 @@
+public class Percolation {
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        int size = N*N;
+        int[] grid = new int[size];
+    }
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j) { }
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+      return true;
+    }
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+      return true;
+    } 
+    // does the system percolate?
+    public boolean percolates() {
+      return true;
+    }   
+}
